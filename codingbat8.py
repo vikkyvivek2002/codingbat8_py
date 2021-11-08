14ec225a11f1d55a3fb8cc508d8c3f92bbfea6b7
vivek vardhan . M <84318895+vikkyvivek2002@users.noreply.github.com>
2021-11-08 22:10:58 +0530

    Add files via upload

---
+++ codingbat8.py
@@ -0,0 +1,8 @@
+def pos_neg(a, b, negative):
+
+  if(a<0 and b<0 and negative):
+    return True
+  if((a<0 and b>0 and not negative)or(a>0 and b<0 and not negative)):
+      return True
+  else:
+    return False
